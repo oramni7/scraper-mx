8538e306e67a7846a283a14ca07f6a2c2ff77832
oramni7 <oramni7@gmail.com>
2026-03-23 17:28:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+app.use(express.static("public"));
+
 const URL = "https://ligue-moto-centre.fr/motocross/";
 
 app.get("/data", async(req, res) => {
